2a045858efc46caaa300804c54ab3e3e00a75d50
CJMinecraft01 <speed2pc2@gmail.com>
2021-08-30 20:28:25 +0100

    Add raw materials as appropriate ore items

--- src/main/java/cjminecraft/oreclumps/data/OCItemTagsProvider.java
+++ src/main/java/cjminecraft/oreclumps/data/OCItemTagsProvider.java
@@ -27,7 +27,40 @@ public class OCItemTagsProvider extends ItemTagsProvider {
                 OCItems.TIN_INGOT.get(),
                 OCItems.OSMIUM_INGOT.get(),
                 OCItems.URANIUM_INGOT.get(),
-                OCItems.ZINC_INGOT.get());
+                OCItems.ZINC_INGOT.get()
+        );
+
+        tag(Tags.Items.ORES).add(
+                OCItems.RAW_IRON.get(),
+                OCItems.RAW_GOLD.get(),
+                OCItems.RAW_COPPER.get(),
+                OCItems.RAW_LEAD.get(),
+                OCItems.RAW_NICKEL.get(),
+                OCItems.RAW_SILVER.get(),
+                OCItems.RAW_TIN.get(),
+                OCItems.RAW_OSMIUM.get(),
+                OCItems.RAW_URANIUM.get(),
+                OCItems.RAW_ZINC.get()
+        );
+
+        tag(Tags.Items.ORES_IRON).add(OCItems.RAW_IRON.get());
+        tag(Tags.Items.ORES_GOLD).add(OCItems.RAW_GOLD.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/copper")))
+                .add(OCItems.RAW_COPPER.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/lead")))
+                .add(OCItems.RAW_LEAD.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/nickel")))
+                .add(OCItems.RAW_NICKEL.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/silver")))
+                .add(OCItems.RAW_SILVER.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/tin")))
+                .add(OCItems.RAW_TIN.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/osmium")))
+                .add(OCItems.RAW_OSMIUM.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/uranium")))
+                .add(OCItems.RAW_URANIUM.get());
+        tag(ItemTags.createOptional(new ResourceLocation("forge", "ores/zinc")))
+                .add(OCItems.RAW_ZINC.get());
 
         tag(ItemTags.createOptional(new ResourceLocation("forge", "ingots/copper")))
                 .add(OCItems.COPPER_INGOT.get());
@@ -45,7 +78,5 @@ public class OCItemTagsProvider extends ItemTagsProvider {
                 .add(OCItems.URANIUM_INGOT.get());
         tag(ItemTags.createOptional(new ResourceLocation("forge", "ingots/zinc")))
                 .add(OCItems.ZINC_INGOT.get());
-
-
     }
 }
